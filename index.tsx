95bfec025d895e8bcccbf0f5d503e24344571a66
kimdu1234 <galaxy2853@gmail.com>
2020-07-25 11:40:09 +0900

    Add includeAllergy checkbox

--- index.tsx
+++ index.tsx
@@ -4,6 +4,9 @@ import Typography from '@material-ui/core/Typography'
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles({
   title: {
@@ -16,11 +19,17 @@ const useStyles = makeStyles({
 function App() {
   const classes = useStyles();
   let [isOpen, setOpen] = React.useState(false);
+  let [includeAllergy, setIncludeAllergy] = React.useState(false);
 
   return (<>
     <CssBaseline />
     <Container>
-      <Typography variant="h4" className={classes.title}>사용자화</Typography>
+    <Typography variant="h4" className={classes.title}>사용자화</Typography>
+      <FormControlLabel
+        control={<Checkbox checked={includeAllergy} onClick={() => setIncludeAllergy(!includeAllergy)} />}
+        label="알레르기 정보 포함"
+      />
+      <TextField></TextField>
     </Container>
   </>)
 }
